c98307c61e31fc703b9915b7af26284a72ff97c5
realingy <754037927@qq.com>
2019-10-22 15:53:23 +0800

    完善cmake命令

---
+++ ch2/pcl_write/pcd_write.cpp
@@ -0,0 +1,37 @@
+#define  _SCL_SECURE_NO_WARNINGS
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h> //pcd 读写类相关的头文件。
+#include <pcl/io/ply_io.h>
+#include <pcl/point_types.h> //PCL中支持的点类型头文件。
+#include <iostream>
+
+int user_data;
+using namespace std;
+
+void viewerOneOff(pcl::visualization::PCLVisualizer & viewer)
+{
+	viewer.setBackgroundColor(1.0, 0.5, 1.0);   //设置背景颜色
+}
+
+int main()
+{
+#if 1
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+	char strfilepath[256] = "rabbit.pcd";
+	if (-1 == pcl::io::loadPCDFile(strfilepath, *cloud)) {
+		cout << "error input!" << endl;
+		return -1;
+	}
+
+	cout << cloud->points.size() << endl;
+	pcl::visualization::CloudViewer viewer("Cloud Viewer"); //创建viewer对象
+
+	viewer.showCloud(cloud);
+	viewer.runOnVisualizationThreadOnce(viewerOneOff);
+	system("pause");
+#endif
+	return 0;
+}
+
