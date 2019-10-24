79e706103f5c99449cca5ad0792a9b3689fd0de4
realingy <754037927@qq.com>
2019-10-24 10:52:32 +0800

    优化代码

--- pcl_module_filter/6_ExtractIndices/提取索引.cpp
+++ pcl_module_filter/6_ExtractIndices/提取索引.cpp
@@ -26,7 +26,7 @@ int main(int argc, char** argv)
 
 	std::cerr << "PointCloud before filtering: " << cloud_blob->width * cloud_blob->height << " data points." << std::endl;
 	
-	// 创建体素栅格下采样: 下采样的大小为1cm
+	//创建体素栅格下采样: 下采样的大小为1cm
 	pcl::VoxelGrid<pcl::PCLPointCloud2> sor; //体素栅格下采样对象
 	sor.setInputCloud(cloud_blob);           //原始点云
 	sor.setLeafSize(0.01f, 0.01f, 0.01f);    //设置采样体素大小
@@ -38,11 +38,9 @@ int main(int argc, char** argv)
 
 	std::cerr << "PointCloud after filtering: " << cloud_filtered->points.size() << " data points." << std::endl;  // 保存下采样后的点云
 
-	//pcl::PCDWriter writer;
-	//writer.write<pcl::PointXYZ>("table_scene_lms400_downsampled.pcd", *cloud_filtered, false);
+	pcl::PCDWriter writer;
+	writer.write<pcl::PointXYZ>("table_scene_lms400_downsampled.pcd", *cloud_filtered, false);
 
-	pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients());
-	pcl::PointIndices::Ptr inliers(new pcl::PointIndices());
 	pcl::SACSegmentation<pcl::PointXYZ> seg;		//创建分割对象
 	seg.setOptimizeCoefficients(true);				//设置对估计模型参数进行优化处理
 	seg.setModelType(pcl::SACMODEL_PLANE);          //设置分割模型类别
@@ -54,38 +52,41 @@ int main(int argc, char** argv)
 	pcl::ExtractIndices<pcl::PointXYZ> extract;		//创建点云提取对象
 	int nr_points = (int)cloud_filtered->points.size();  // While 30% of the original cloud is still there
 
+	pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients()); //模型系数对象
+	pcl::PointIndices::Ptr inliers(new pcl::PointIndices()); //点云索引对象
+	int i = 0;
 	while (cloud_filtered->points.size() > 0.3 * nr_points)
 	{
-		// 为了处理点云包含的多个模型，在一个循环中执行该过程并在每次模型被提取后，保存剩余的点进行迭代
+		// 为了处理点云包含的多个模型，在一个循环中执行该过程并在每次模型被提取后，保存剩余的点
 		seg.setInputCloud(cloud_filtered);
 		seg.segment(*inliers, *coefficients);
 		if (inliers->indices.size() == 0)
 		{
-			std::cerr << "Could not estimate a planar model for the given dataset." << std::endl;      break;
+			std::cerr << "Could not estimate a planar model for the given dataset." << std::endl;
+			break;
 		}
 
 		// Extract the inliers  
 		extract.setInputCloud(cloud_filtered);
 		extract.setIndices(inliers);
-		extract.setNegative(false);
+		extract.setNegative(false); // false: 保留索引点，去除其他部分
 		extract.filter(*cloud_p);
-		std::cerr << "PointCloud representing the planar component: " << cloud_p->width * cloud_p->height << " data points." << std::endl;
+		std::cerr << "PointCloud representing the planar component: " << cloud_p->points.size() << " data points." << std::endl;
 		std::stringstream ss;
-		// ss << "table_scene_lms400_plane_" << i << ".pcd";
-		// writer.write<pcl::PointXYZ>(ss.str(), *cloud_p, false);
+		ss << "table_scene_lms400_plane_" << i << ".pcd";
+		writer.write<pcl::PointXYZ>(ss.str(), *cloud_p, false);
 
 		// Create the filtering object
-		extract.setNegative(true);
+		extract.setNegative(true); // true: 去除索引点，保留其他部分
 		extract.filter(*cloud_f);
 		cloud_filtered.swap(cloud_f);
+		i++;
 	}
 
 	//pcl::visualization::CloudViewer viewer("cloud"); //创建viewer对象
 	//viewer.showCloud(cloud);
 	pcl::visualization::CloudViewer viewer2("cloud_filtered"); //创建viewer对象
 	viewer2.showCloud(cloud_filtered);
-
-	//viewer.showCloud(cloud_f);
 
 	system("pause");
 	return (0);
