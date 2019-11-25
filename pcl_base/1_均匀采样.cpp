b86f1dc991abee3e21d4f411402ee3ae3f5ae3b7
realingy <754037927@qq.com>
2019-11-25 20:35:57 +0800

    点云查看

--- pcl_base/1_均匀采样.cpp
+++ pcl_base/1_均匀采样.cpp
@@ -6,10 +6,13 @@ int main(int argc, char** argv)
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 	pcl::PointCloud<pcl::PointXYZ>::Ptr filteredCloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-	if (pcl::io::loadPCDFile<pcl::PointXYZ>("rabbit.pcd", *cloud) != 0)
+	//if (pcl::io::loadPCDFile<pcl::PointXYZ>("rabbit.pcd", *cloud) != 0)
+	if (pcl::io::loadPCDFile<pcl::PointXYZ>("20160426_CALIB.pcd", *cloud) != 0)
 	{
 		return -1;
 	}
+
+	std::cout << "count : " << cloud->points.size() << "before filter" << std::endl;
 
 	// Uniform sampling object.
 	pcl::UniformSampling<pcl::PointXYZ> filter;
@@ -19,6 +22,8 @@ int main(int argc, char** argv)
 	pcl::PointCloud<int> keypointIndices;
 	// filter.compute(keypointIndices);
 	pcl::copyPointCloud(*cloud, keypointIndices.points, *filteredCloud);
+
+	std::cout << "count: " << cloud->points.size() << "after filter" << std::endl;
 	
 	return 0;
 }
